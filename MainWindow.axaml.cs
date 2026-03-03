7e3a396068eab889128baba63d3a5c5adaacc198
Z9TONEDEVELOPER <maxalex1018@mail.ru>
2026-03-03 11:18:52 +0300

    fix

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.ObjectModel;
 using Avalonia.Controls;
-using Avalonia.Interactivity;           // для Loaded
+using Avalonia.Interactivity;          
 using System.ComponentModel;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
